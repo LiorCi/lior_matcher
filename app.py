67e6ab693e43c9a93c6c98306565afe3db4d14e6
roy <roy@taykey.com>
2020-02-16 23:45:22 +0200

    fixed pep8 new line

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask_admin import Admin
 
-
 from matcher import create_app, model_view, db, models
 
 if __name__ == '__main__':
     app = create_app()
-
-### end swagger specific ###
 
     # Flask admin
     admin = Admin(app, url=app.config['FLASK_ADMIN_URL'], name='matcher', template_mode='bootstrap3')
